f76ea6ea2ddb4dea0963cc25c003f111df1445f4
narendra <narendra.anr@gmail.com>
2020-03-12 23:40:14 -0400

    Added spring boot initial commit

---
+++ src/main/java/com/mkyong/TableauTicketRequestDTO.java
@@ -0,0 +1,20 @@
+package com.mkyong;
+
+public class TableauTicketRequestDTO {
+
+    private String url;
+    public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	private String userName;
+    
+}
