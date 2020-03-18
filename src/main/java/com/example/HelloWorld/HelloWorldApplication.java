bb627f2e373a9ee396b33148034d4a42cda946ee
narendra <narendra.anr@gmail.com>
2020-03-18 15:40:58 -0400

    commit

--- src/main/java/com/example/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorld/HelloWorldApplication.java
@@ -1,9 +1,13 @@
 package com.example.HelloWorld;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +18,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @RestController
 public class HelloWorldApplication extends SpringBootServletInitializer
+
 {
+	 private static final Logger logger = LogManager.getLogger(HelloWorldApplication.class);
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
 	{
@@ -27,22 +33,35 @@ public class HelloWorldApplication extends SpringBootServletInitializer
 	}
 
 	@RequestMapping(value = "/hello")
-	public String helloWorld(@RequestParam String username,@RequestParam String url)
+	public ResponseEntity<String> helloWorld(@RequestParam String username,@RequestParam String url,@RequestParam String site,@RequestParam String client_Ip)
 	{
 		
 		 RestTemplate restTemplate = new RestTemplate();
 		    //String result = restTemplate.getForObject(dto.getUrl(), String.class);
 		    System.out.println("USER DTo :"+username);
+		    logger.info("Service Inputs :userName"+username,"url="+url,"site="+site);
 		    MultiValueMap<String, String> parametersMap = new LinkedMultiValueMap<String, String>();
 		    parametersMap.add("username", username);
+		    parametersMap.add("target_site",site);
+		    parametersMap.add("client_Ip",client_Ip);
 		    
 		   // ResponseEntity<String> result = restTemplate.exchange(dto.getUrl(), HttpMethod.POST, entity, String.class);
 		    
 		    String result = restTemplate.postForObject(url, parametersMap, String.class);
 		     
 		    System.out.println(result);
-		   
-		
-		return username;
+		    logger.info("Token :",result);
+		    String urlToBeRedirected = null;
+		    if(null != result & !("-1".equals(result))) {
+		    	
+		    	//http://tabserver/trusted/<ticket>/views/<workbook>/<view> 
+		    	 urlToBeRedirected = url + "/" + result +  "/t/ELTScorecard/views/" + "ELTScorecard" + "/" + "Home";
+		    	System.out.println("url to be redirected " + urlToBeRedirected);
+
+		    	
+		    	
+		    }
+		    return new ResponseEntity<String>(urlToBeRedirected, HttpStatus.OK);
+		//return result;
 	}
 }
